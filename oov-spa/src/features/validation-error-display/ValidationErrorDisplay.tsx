a54a6f027499cf2baa81d83009f2b51e43c4f066
Jevgeni Tarasov <jevgeni@hey.com>
2021-02-07 19:30:18 +0100

    Update ValidationErrorDisplay

--- oov-spa/src/features/validation-error-display/ValidationErrorDisplay.tsx
+++ oov-spa/src/features/validation-error-display/ValidationErrorDisplay.tsx
@@ -1,4 +1,4 @@
-import { JSX, createSignal } from 'solid-js'
+import { JSX, createSignal, createMemo, createEffect, on } from 'solid-js'
 import { ValidationResult } from '../../app/api'
 import { ValidationErrorList } from './ValidationErrorList'
 import { ValidationErrorStatistics } from './ValidationErrorStatistics'
@@ -9,6 +9,16 @@ export interface ValidationErrorDisplayProperties {
 
 export function ValidationErrorDisplay<T extends ValidationErrorDisplayProperties>(props: T): JSX.Element {
   const [selectedErrorId, setErrorId] = createSignal(undefined as string | undefined)
+
+  //The effect below is done, so that the <ValidationErrorList /> disappears
+  //after we switch to another validation result, otherwise it might be a bit
+  //confusing to the users
+  const validationResult = createMemo(() => props.validationResults)
+  createEffect(
+    on(validationResult, (_) => {
+      setErrorId(undefined)
+    })
+  )
   return (
     <>
       <ValidationErrorStatistics
