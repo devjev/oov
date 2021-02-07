81d0742fc9393f2cb321e125c5392e5b1629673e
Jevgeni Tarasov <jevgeni@hey.com>
2021-02-07 01:04:18 +0100

    Add sidebar layout and rethink styling organization

---
+++ oov-spa/src/ui/inline-monospace/InlineMonospace.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'solid-js/jsx-runtime'
+import { ChildElement } from '../prelude'
+
+export interface InlineMonospaceProperties {
+  children?: ChildElement[] | ChildElement
+}
+
+export function InlineMonospace<T extends InlineMonospaceProperties>(props: T): JSX.Element {
+  return <span class="inline-monospace">{props.children}</span>
+}
